55860ae4fd056d388857fff09acd407793c3fb47
LethaboK <lethabokhedama@gmail.com>
2026-08-12 03:53:57 +0000

    (fix): align types package tsconfig to NodeNext to match server compilation

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,13 +1,13 @@
 // Domain
-export * from './domain/user.types';
-export * from './domain/message.types';
-export * from './domain/call.types';
+export * from './domain/user.types.js';
+export * from './domain/message.types.js';
+export * from './domain/call.types.js';
 
 // API
-export * from './api/auth.types';
-export * from './api/messaging.types';
-export * from './api/media.types';
-export * from './api/calls.types';
+export * from './api/auth.types.js';
+export * from './api/messaging.types.js';
+export * from './api/media.types.js';
+export * from './api/calls.types.js';
 
 // Socket
-export * from './socket/events.types';
+export * from './socket/events.types.js';
